07c42c87e2fc1aa1eca6c9d884760aaf724b6c75
fumikito <guy@hametuha.com>
2024-07-03 16:11:14 +0900

    リーグは基本リーグの紐づいたpostを出すページ、設定で例外を適用可能

--- app/Tarosky/Common/Hooks/LeagueRegisterHooks.php
+++ app/Tarosky/Common/Hooks/LeagueRegisterHooks.php
@@ -11,10 +11,16 @@ use Tarosky\Common\Pattern\HookPattern;
 class LeagueRegisterHooks extends HookPattern {
 
 	/**
+	 * @var string リーグのクエリで優先すべきもの
+	 */
+	protected static $prior_post_type = 'post';
+
+	/**
 	 * {@inheritDoc}
 	 */
 	protected function register_hooks(): void {
 		add_action( 'init', [ $this, 'register_league' ] );
+
 		add_action( 'pre_get_posts', [ $this, 'pre_get_posts'] );
 	}
 
@@ -39,6 +45,18 @@ class LeagueRegisterHooks extends HookPattern {
 	}
 
 	/**
+	 * リーグのクエリで優先すべきものを設定する
+	 *
+	 * @param string $post_type
+	 * @return void
+	 */
+	public static function set_prior_post_type( $post_type ) {
+		if ( in_array( $post_type, [ 'post', 'team' ], true ) ) {
+			self::$prior_post_type = $post_type;
+		}
+	}
+
+	/**
 	 * リーグのクエリを変更する
 	 *
 	 * @param \WP_Query $wp_query
@@ -46,12 +64,25 @@ class LeagueRegisterHooks extends HookPattern {
 	 */
 	public function pre_get_posts( &$wp_query ) {
 		if ( $wp_query->is_main_query() && $wp_query->is_tax( 'league' ) ) {
-			// postが入ってしまっていると、全件出てしまうので絞る。
-			$wp_query->set( 'post_type', 'team' );
-			// 親カテゴリーがある場合（ヨーロッパ > プレミアリーグ）は全件表示
-			$tax = get_queried_object();
-			if ( $tax->parent ) {
-				$wp_query->set( 'posts_per_page', - 1 );
+			// スタッツを除外
+			if ( sk_is_stat() ) {
+				return;
+			}
+			switch ( self::$prior_post_type ) {
+				case 'team':
+					// postが入ってしまっていると、全件出てしまうので絞る。
+					$wp_query->set( 'post_type', 'team' );
+					// 親カテゴリーがある場合（ヨーロッパ > プレミアリーグ）は全件表示
+					$tax = get_queried_object();
+					if ( $tax->parent ) {
+						$wp_query->set( 'posts_per_page', - 1 );
+					}
+					break;
+				case 'post':
+				default:
+					// teamを除外する
+					$wp_query->set( 'post_type', 'post' );
+					break;
 			}
 		}
 	}
